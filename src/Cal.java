0eb07e092c7d96b0910513ffab0308d28c9fba93
simplicity-load <simplicity_load@tutanota.com>
2022-05-21 23:13:44 +0200

    NOT WORKING: trying to fix the algorithm being off by a couple of days off

--- src/Cal.java
+++ src/Cal.java
@@ -3,34 +3,80 @@ public class Cal {
     public Cal() { }
 
     //TODO get only the specified month
+    /*
     public Year[] getMonth(int month, int year)
     {
         return getRangeOfMonths(month, year, month, year);
     }
+    */
+
+
+    // How the first day of the first year starts out
+    private static final int initial_day = 1;
+    private static final int initial_week_position = 6;
+    private static final int initial_month = 1;
+    private static final int initial_year = 1;
 
     //TODO get a range of months including BEGIN and END limits
-    public Year[] getRangeOfMonths(int beginMonth, int beginYear, int endMonth, int endYear)
+    //public Year[] getRangeOfMonths(int beginMonth, int beginYear, int endMonth, int endYear)
+    public void getRangeOfMonths(int beginMonth, int beginYear, int endMonth, int endYear)
     {
-        if (beginMonth < 0 ||
-            beginYear < 0 ||
-            endMonth < 0 ||
-            endYear < 0 ||
-            beginMonth > endYear ||
-            (beginMonth < endMonth && beginYear == endYear)) {
+        if (beginMonth < 0 || endMonth < 0 ||
+            beginMonth > 12 || endMonth > 12 ||
+            beginYear < 0 || endYear < 0 ||
+            beginYear > endYear ||
+            (beginMonth > endMonth && beginYear == endYear)) {
 
             System.out.println("You inputted wrong actual parameters for the Calendar");
             System.exit(1);
         }
+        //System.out.printf("beginYear: %d | endYear: %d | beginYear > endYear: %s\n", beginYear, endYear, beginYear > endYear);
+        // We add one since were going to want to inclusively calculate years
+        int number_of_years = endYear - beginYear + 1;
+        Year[] yearArray = new Year[number_of_years];
 
-        // How the first day of the first year starts out
-        int intialDay = 1;
-        int initialYear = 1;
-        int initialWeekPosition = 5;
-        return new Year[] {new Year(2022)};
+
+        int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int countDay = initial_day;
+        int countWeekPosition = initial_week_position;
+        int countMonth = initial_month;
+        int countYear = initial_year;
+        int counterr = 0;
+        while (countMonth < beginMonth || countYear < beginYear)
+        {
+            if ((countYear % 4 == 0 && countYear % 100 != 0) ||
+                countYear % 400 == 0)
+                daysInMonth[1] = 29;
+            else
+                daysInMonth[1] = 28;
+
+            if (countDay == daysInMonth[countMonth-1]) {
+                if (countMonth == 12) {
+                    countYear++;
+                    countMonth = 1;
+                }
+                else
+                    countMonth++;
+                countDay = 1;
+            } else
+                countDay++;
+
+            if (countWeekPosition == 7)
+                countWeekPosition = 1;
+            else
+                countWeekPosition++;
+            counterr++;
+        }
+
+        //while ( countYear
+        System.out.printf("countDay: %d | countWeekPosition: %d | countMonth: %d | countYear: %d | counter: %d\n", countDay, countWeekPosition, countMonth, countYear, counterr);
+
+        //return yearArray;
     }
 
     public static void main(String[] args)
     {
+        /*
         Day someday = new Day(1, 30);
         Day someday1 = new Day(2, 31);
         Day someday2 = new Day(3, 32);
@@ -54,6 +100,22 @@ public class Cal {
                         System.out.println(d.getDay());
                     else
                         System.out.println("null");
+        */
+        Cal col = new Cal();
+        col.getRangeOfMonths(2, 1, 3, 2022);
+        col.getRangeOfMonths(3, 1, 3, 2022);
+        col.getRangeOfMonths(4, 1, 3, 2022);
+        col.getRangeOfMonths(5, 1, 3, 2022);
+        col.getRangeOfMonths(6, 1, 3, 2022);
+        col.getRangeOfMonths(7, 1, 3, 2022);
+        col.getRangeOfMonths(8, 1, 3, 2022);
+        col.getRangeOfMonths(9, 1, 3, 2022);
+        col.getRangeOfMonths(10, 1, 3, 2022);
+        col.getRangeOfMonths(11, 1, 3, 2022);
+        col.getRangeOfMonths(12, 1, 3, 2022);
+        col.getRangeOfMonths(1, 2, 3, 2022);
+        col.getRangeOfMonths(2, 2, 3, 2022);
+        col.getRangeOfMonths(3, 2, 3, 2022);
 
     }
 }
